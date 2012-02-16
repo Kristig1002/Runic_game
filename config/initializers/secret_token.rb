c6d745430603e83f4af64d6851324da3aeac1614
Kristine Griesbach <kristine@obliqueideas.com>
2012-02-16 16:11:39 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RunicGame::Application.config.secret_token = '89083ec7ebe3700af37d29938b6e7c0985226be0c0ea418feb140ea428ef0d3c6efce4a1e7721d18ee0d457fd3733c0ca6a4eeff6f397b2af49dcaedd08fe99e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RunicGame::Application.config.session_store :cookie_store, key: '_runic_game_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RunicGame::Application.config.session_store :active_record_store
